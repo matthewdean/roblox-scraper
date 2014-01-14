ff9fc8bab4fba00eed84caf82de6738415c68bf8
Matthew Dean <seranok@gmail.com>
2014-01-14 01:57:43 -0600

    Renamed getAssetsFromProfile to getAssetsOwnedByUser
    getAssetsOwnedByUser works with username or user ID

--- README.md
+++ README.md
@@ -6,28 +6,44 @@ roblox-scraper
 ```javascript
 var roblox = require('roblox-scraper')
 
-roblox.getUserId('builderman', function(err, userId) {
-  console.log(userId) // 156
-  roblox.getAssetsFromProfile(userId, 'Heads', function(heads) {
-    console.log(heads)
-  })
-})
+// this method takes username or user ID as its first argument
+roblox.getAssetsOwnedByUser('shedletsky', 'hats', 1, function(result) {
+  console.log('shedletsky has ' + result.totalAssets + ' hats'); // 1080
+  var limitedUniques = result.assets.filter(function(hat) {
+	return hat.isLimitedUnique;
+  });
+  limitedUniques.forEach(function(limited) {
+    console.log('#' + limited.serialNumber + '/' + limited.totalSold + ' ' + limited.name);
+  });
+});
 ```
 
 Output:
 
-```javascript
-[{
-  name: 'Barrel',
-  id: 6340170,
-  priceInRobux: 800,
-  creatorName: 'ROBLOX'
-}, {
-  name: 'Flat Top',
-  id: 6340208,
-  priceInRobux: 50,
-  creatorName: 'ROBLOX'
-}]
+```
+	shedletsky has 1080 hats
+	#237/300 Mr X
+	#170/300 Mr X
+	#43/300 Mr X
+	#411/500 Mr. Fancy Top Hat
+	#29/500 Mr. Fancy Top Hat
+	#1754/2500 Sapphire Vision Visor
+	#1490/2500 Sapphire Vision Visor
+	#3249/5000 Noob Attack: Periastron Punishment
+	#1418/5000 Noob Attack: Periastron Punishment
+	#1329/5000 Noob Attack: Periastron Punishment
+	#1198/5000 Noob Attack: Periastron Punishment
+	#835/5000 Noob Attack: Periastron Punishment
+	#3706/10000 Sk12r Boi
+	#460/10000 Sk12r Boi
+	#1326/2000 Raig Tree
+	#159/2000 Raig Tree
 ```
 
+Get all of a user's models:
 
+```javascript
+roblox.getAssetsOwnedByUser('gusmanak', 'models', function(result) {
+  console.log(result.assets[0]) // { name: 'Pain Region Brick', id: 141537523, creatorName: 'stickmasterluke' }
+})
+```
--- index.js
+++ index.js
@@ -3,37 +3,61 @@ var requestWithEncoding = require('./lib/request-with-encoding.js'),
 	profile = require('./lib/profile.js'),
 	cheerio = require('cheerio');
 
-exports.getAssetsFromProfile = function(userId, assetType, callback) {
+
+var getAssetsOwnedByUser = function(userId, assetType, maxPages, callback) {
 	var url = 'http://www.roblox.com/user.aspx?id=' + userId;
 	var assets = [];
+	var totalAssets;
+	
 	requestWithEncoding({ method: 'GET', url: url }, function(err, res, body) {
 		var $ = cheerio.load(body);
-		var controls = {
-			__EVENTTARGET: $('.verticaltab > a').filter(function() { return $(this).text() == assetType; }).attr('href').match(/"([^"]+)/)[1],
-			__VIEWSTATE: $('#__VIEWSTATE').val(),
-			__EVENTVALIDATION: $('#__EVENTVALIDATION').val(),
-			__VIEWSTATEENCRYPTED: ''
+		var correctAssetType = function() {
+			return $(this).text().toLowerCase() == assetType.toLowerCase();
 		};
-		requestWithEncoding({ method: 'POST', url: url, form: controls }, function onResponse(err, res, body) {
+		
+		function onResponse(err, res, body) {
+			maxPages--;
 			var $ = cheerio.load(body);
 			var page = new profile($);
 			assets = assets.concat(page.assets);
-			if (page.currentPage < page.totalPages) {
-				var controls = {
-					__EVENTTARGET: 'ctl00$cphRoblox$rbxUserAssetsPane$FooterPageSelector_Next',
-					__VIEWSTATE: $('#__VIEWSTATE').val(),
-					__EVENTVALIDATION: $('#__EVENTVALIDATION').val(),
-					__VIEWSTATEENCRYPTED: ''
-				};
-				requestWithEncoding({ method: 'POST', url: url, form: controls }, onResponse);
-			} else {
-				callback(assets);
+			if (page.currentPage == 1) {
+				var assetsPerPage = 18;
+				totalAssets = page.assets.length + (page.totalPages * assetsPerPage);
 			}
-		});
+			if (page.currentPage >= page.totalPages || maxPages == 0) {
+				callback({ totalAssets: totalAssets, pages: page.currentPage, totalPages: page.totalPages, assets: assets });
+				return;
+			}
+			var controls = {
+				__EVENTTARGET: 'ctl00$cphRoblox$rbxUserAssetsPane$FooterPageSelector_Next',
+				__VIEWSTATE: $('#__VIEWSTATE').val(),
+				__EVENTVALIDATION: $('#__EVENTVALIDATION').val(),
+				__VIEWSTATEENCRYPTED: ''
+			};
+			requestWithEncoding({ method: 'POST', url: url, form: controls }, onResponse);
+		}
+		
+		var button = $('.verticaltab > a').filter(correctAssetType);
+		if (button.length == 0) {
+			throw new Error('invalid asset type');
+		}
+		if (button.hasClass('selected')) {
+			// we're already on the right page (Hats)
+			onResponse(err, res, body);
+		} else {
+			var eventTarget = button.attr('href').match(/"([^"]+)/)[1];
+			var controls = {
+				__EVENTTARGET: eventTarget,
+				__VIEWSTATE: $('#__VIEWSTATE').val(),
+				__EVENTVALIDATION: $('#__EVENTVALIDATION').val(),
+				__VIEWSTATEENCRYPTED: ''
+			};
+			requestWithEncoding({ method: 'POST', url: url, form: controls }, onResponse);
+		}
 	});
 };
 
-exports.getUserId = function(username, callback) {
+var getUserId = function(username, callback) {
 	request({
 		method: 'HEAD',
 		url: 'http://www.roblox.com/user.aspx?username=' + username,
@@ -51,4 +75,20 @@ exports.getUserId = function(username, callback) {
 		userId = parseInt(userId);
 		callback(null, userId);
 	});
+};
+
+exports.getUserId = getUserId;
+
+exports.getAssetsOwnedByUser = function(user, assetType, maxPages, callback) {
+	if (typeof callback == "undefined") {
+		callback = maxPages;
+		maxPages = 1;
+	}
+	if (typeof user == 'number') {
+		getAssetsOwnedByUser(userId, assetType, maxPages, callback);
+	} else {
+		getUserId(user, function(err, userId) {
+			getAssetsOwnedByUser(userId, assetType, maxPages, callback)
+		})
+	}
 };
